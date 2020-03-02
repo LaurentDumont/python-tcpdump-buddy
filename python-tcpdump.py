a977135e0937345232bd92c057a117e1db192cc6
Laurent Dumont <laurentfdumont@gmail.com>
2020-03-02 18:48:22 -0500

    Update python-tcpdump.py
    Add custom text fields

--- python-tcpdump.py
+++ python-tcpdump.py
@@ -3,10 +3,12 @@ import sys
 import subprocess
 
 # PyQt5 imports
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 
+# Build pyinstaller .exe
+# pyinstaller --onefile --noconsole --paths venv\Lib\site-packages python-tcpdump.py
 
 class App(QMainWindow):
 
@@ -30,27 +32,45 @@ class App(QMainWindow):
 
         # Create a text field for the target compute
         self.target_text = QLineEdit(self, placeholderText="Target server to dump")
-        self.target_text.move(20, 80)
+        self.target_text.move(20, 60)
         self.target_text.resize(280,40)
 
         # Create a text field for the tcpdump filter
-        self.filter_text = QLineEdit(self, placeholderText="TCPDUMP filter syntax (auto ignore for tcp port 22 - ssh traffic)")
-        self.filter_text.move(20, 140)
-        self.filter_text.resize(280,40)
+        self.tcpdump_filter_text = QLineEdit(self, placeholderText="TCPDUMP filter syntax (auto ignore for tcp port 22 - ssh traffic)")
+        self.tcpdump_filter_text.move(20, 100)
+        self.tcpdump_filter_text.resize(280,40)
+
+        # Create a text field for the SSH username
+        self.username_text = QLineEdit(self, placeholderText="SSH username")
+        self.username_text.move(20, 140)
+        self.username_text.resize(280,40)
 
         button = QPushButton('Start tcpdump', self)
         button.setToolTip('Dump all the traffic!')
+        button.move(20,220)
         button.resize(280,40)
-        button.move(20,200)
         button.clicked.connect(self.on_click)
-
+        
         self.show()
 
     @pyqtSlot()
     def on_click(self):
-        process = subprocess.Popen(['wireshark'],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
+        jumpbox = self.jumpbox_text.text()
+        target = self.target_text.text()
+        username = self.username_text.text()
+        tcpdump_filter = self.tcpdump_filter_text.text()
+        print(tcpdump_filter)
+        plinkCommand = [r"C:\Program Files\PuTTY\plink", 
+        "-batch", 
+        "-proxycmd", 
+        r"C:\Program Files\PuTTY\plink "+username+"@"+jumpbox+" -pw test123 -nc "+target+":22", 
+        username+"@"+target, 
+        "-pw", 
+        "test123", 
+        r"sudo tcpdump -enni any -l -s 0 -w - not port 22 and "+tcpdump_filter, 
+        "|", 
+        r'C:\Program Files\Wireshark\Wireshark.exe', "-k", "-i", "-"]
+        process = subprocess.Popen(plinkCommand, shell=True)
         stdout, stderr = process.communicate()
 
 if __name__ == '__main__':
